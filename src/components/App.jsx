14379eb7bf1acfb9e13e39e844a005e152880f89
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-08-15 04:54:13 +0300

    231

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,100 +1,19 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import Form from "./Phonebook/PhonebookForm";
-import PhoneBookList from "./PhonebookList/PhoneBookList";
-import { nanoid } from 'nanoid'
-import Filter from "./Filter/Filter";
-import Notiflix from 'notiflix';
-import { saveToLocalStorage, loadContacts } from "./hooks/useLocalStorage";
+import React from 'react';
+import Head from './Title/head';
+import Section from './Title/section-title';
+import Contacts from './Contacts/contacts';
+import SearchContact from './SearchContact/SearchContact';
+import AddContactForm from './Add-contact/add-contact';
 
-
-
-function App () {
- 
-const [contacts, setContacts] = useState([]);
-const [filter, setFilter] = useState('')
-
-
-
-useEffect(() => {
-  const savedContacts = loadContacts('contacts');
-  if (savedContacts && savedContacts.length > 0) {
-    setContacts(savedContacts);
-  }
-}, []);
-
-
-useEffect(() => {
-  saveToLocalStorage('contacts', contacts);
-}, [contacts]);
-
-
-
-const addContact = (name, number) => {
-    
-   const idCont = nanoid()
-   const arrayinfo = { name: name, number: number, id: idCont };
-   const checkName  = contacts.find(contact => contact.name === arrayinfo.name);
-   const checkNumbers   = contacts.find(contact => contact.number === arrayinfo.number);
-   if (checkName && checkNumbers) {
-    Notiflix.Report.failure( 
-      `${name} and ${number} is already in contacts`);
-    return
-  } else if (checkName) {
-    Notiflix.Report.failure( 
-      `${name} is already in contacts`);
-    return
-  } else if (checkNumbers){
-    Notiflix.Report.failure( 
-      `${number} is already in contacts`);
-    return
-  }
-   
-   setContacts(prevContacts => [...prevContacts, arrayinfo])
-
- };
-
-
-const  deleteContact = contactId => 
-  setContacts(prevContacts =>{ 
-    return  prevContacts.filter(contact => contact.id !== contactId)},
-  )
-
-
-
- const changeFilter = e => {
-  const searchValue = e.currentTarget.value
-    setFilter(searchValue)
-  };
-
-
-
-
-const getVisibleContacts = () => {
-  
-  const normalizedFilter = filter.toLowerCase();
-
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter))
-  
-};
-  
-
+export const App = () => {
   return (
-    <div> 
-    <h2>Phonebook</h2>
-
-    <Form addContact={addContact}></Form>
-    
-    <Filter value={filter} onChange={changeFilter} />
-
-    <h2>Contacts</h2>
-    
-    <PhoneBookList 
-    contacts={contacts} 
-    visibleContacts={getVisibleContacts()} onDeleteContacts={deleteContact} />
-</div>
+    <div className="container">
+      <Head headTitle="Phonebook" />
+      <AddContactForm />
+      <Section title="Contacts">
+        <SearchContact searchTitle="Find contacts by name" />
+        <Contacts />
+      </Section>
+    </div>
   );
-}
-
-export default App
+};
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+
 import css from './Filter.module.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { updateFilter } from 'redux/reducers/filterSlice';
+import { selectorContacts } from 'redux/selectors';
 
-const Filter = ({ value, onChange }) => {
-return (
-<label className={css.filterbox}>
-    <samp className={css.textfilter}>Find contacts by name</samp> 
-    <input 
-    className={css.inputfilter}
-    type="text" 
-    value={value} onChange={onChange} />
-  </label>)}
+const SearchContact = ({ searchTitle }) => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectorContacts);
 
+  const handleSearchInput = e => {
+    dispatch(updateFilter(e.target.value.trim()));
+  };
 
-export default Filter;
+  return (
+    contacts.length > 0 && (
+      <div className={css.wrapper}>
+        <h3 className={css.title}>{searchTitle}</h3>
+        <input className={css.input} type="text" onInput={handleSearchInput} />
+      </div>
+    )
+  );
+};
+
+export default SearchContact;
